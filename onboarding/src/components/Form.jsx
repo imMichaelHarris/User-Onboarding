a223b83e890884e31e276e61e60dd6bdbc5e9ce4
Michael Harris <mhguitar1997@gmail.com>
2019-07-17 12:35:58 -0700

    Add form fields from formik

--- onboarding/src/components/Form.jsx
+++ onboarding/src/components/Form.jsx
@@ -3,7 +3,11 @@ import { withFormik, Form, Field } from "formik";
 const Form = () => {
     return (
         <Form>
-
+            <Field type="text" name="name" placeholder="Enter your name" />
+            <Field type="email" name="email" placeholder="Enter your email" />
+            <Field type="password" name="password" placeholder="Enter your password" />
+            <Field type="checkbox" name="tos" />
+            <button>Submit</button>
         </Form>
     );
 };
