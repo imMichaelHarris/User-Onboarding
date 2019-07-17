a548f5b6b4d491f8330acefad2c332c69973fc46
Michael Harris <mhguitar1997@gmail.com>
2019-07-17 12:49:19 -0700

    Import Yup in UserForm

--- onboarding/src/components/UserForm.jsx
+++ onboarding/src/components/UserForm.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 const UserForm = () => {
-    return (
-        <Form>
-            <Field type="text" name="name" placeholder="Enter your name" />
-            <Field type="email" name="email" placeholder="Enter your email" />
-            <Field type="password" name="password" placeholder="Enter your password" />
-            <Field type="checkbox" name="tos" />
-            <button>Submit</button>
-        </Form>
-    );
+  return (
+    <Form>
+      <Field type="text" name="name" placeholder="Enter your name" />
+      <Field type="email" name="email" placeholder="Enter your email" />
+      <Field
+        type="password"
+        name="password"
+        placeholder="Enter your password"
+      />
+      <Field type="checkbox" name="tos" />
+      <button>Submit</button>
+    </Form>
+  );
 };
 
 export default withFormik({
-    mapPropsToValues({name, email, password, tos}){
-        return {
-            name: name || "",
-            email: email || "",
-            password: password || "",
-            tos: tos || ""
-        }
-    },
-    handleSubmit(values){
-        console.log(values);
-    }
+  mapPropsToValues({ name, email, password, tos }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      tos: tos || ""
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
 })(UserForm);
